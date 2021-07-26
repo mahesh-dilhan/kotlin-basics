5968d91398115b1952a93d9ee671ca9132a494ba
mahesh <mail.mahesh.lk@gmail.com>
2021-07-26 20:25:09 +0800

    kt-inheritance

--- src/main/kotlin/InheritanceKt.kt
+++ src/main/kotlin/InheritanceKt.kt
@@ -1,2 +1,20 @@
-class InheritanceKt {
+open class Country {                // 1
+    open fun produceVaccine(name: String, flag: Boolean) {       // 2
+        println("${name} - ${flag}")
+    }
+}
+
+class Pacific : Country() {       // 3
+    override fun produceVaccine(name: String, flag: Boolean) {   // 4
+        println("Pacific region ${name} - ${flag}")
+    }
+}
+
+fun main() {
+    val usa: Country = Pacific()
+    usa.produceVaccine("USA", true)
+
+    val china: Country = Country()
+    china.produceVaccine("China", true)
+
 }
